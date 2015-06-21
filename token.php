c94d8a0d822019e116bdbe00dd6a7baa0360a989
racacax <racacax@gmail.com>
2015-06-21 12:49:34 +0200

    Update token.php

--- token.php
+++ token.php
@@ -4,7 +4,7 @@ Ce script permet de protéger vos scripts PHP par des tokens de protection. Pour
 $login = 'asiojmszdxjomqdjmoidcqjzoiddojijdzoadk';
 $password = 'ZDZ68FE4FE8S4DC5DS1E8EZZ1DDZ';
 $ip = $_SERVER['REMOTE_ADDR'];
-$token = $login.$password.$ip;
+$token = md5($login.$password.$ip);
 echo $token;
 ?>
 Vous accèderez au script ci dessous comme ceci : script.php?token=le token généré dans le fichier ci dessus.
@@ -14,14 +14,14 @@ Vous accèderez au script ci dessous comme ceci : script.php?token=le token gén
 $login = 'asiojmszdxjomqdjmoidcqjzoiddojijdzoadk';
 $password = 'ZDZ68FE4FE8S4DC5DS1E8EZZ1DDZ';
 $ip = $_SERVER['REMOTE_ADDR'];
-$token = $login.$password.$ip;
+$token = md5($login.$password.$ip);
        if(isset($_GET["token"]) && ($_GET["token"] == $token))
         {
 ?>
 le code à protéger
 <?
- } else { echo <html><head><meta content="text/html; charset=utf-8" http-equiv="Content-Type"></head><body><h1 style="height: 25px;">403 Forbidden</h1><hr style="height: 1px;"><h3 style="line-height: 2px;">Pourquoi ce message s\'affiche ?</h3><p>Il se peut que le token ait expiré ou que votre IP ait changé. Allez à l\'écran d\'accueil et cliquez sur Actualiser. Si les problèmes persistent, contactez-moi.</p></body></html> ;
-    // sinon, le formulaire s'affiche
+ } else { echo '<html><head><meta content="text/html; charset=utf-8" http-equiv="Content-Type"></head><body><h1 style="height: 25px;">403 Forbidden</h1><hr style="height: 1px;"><h3 style="line-height: 2px;">Pourquoi ce message s\'affiche ?</h3><p>Il se peut que le token ait expiré ou que votre IP ait changé. Allez à l\'écran d\'accueil et cliquez sur Actualiser. Si les problèmes persistent, contactez-moi.</p></body></html>' ;
+    // sinon, le formulaire s\'affiche
     }
 	
 	}
